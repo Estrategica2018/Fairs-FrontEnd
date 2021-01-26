4130094b79c338206d9c2d0025ee4a4e1f8d033d
David Camacho <davithc01@gmail.com>
2021-01-25 23:43:58 -0500

    adicion de pagina de noticias

--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -10,6 +10,24 @@ export class NewsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+	  
+	document.getElementsByClassName('container')[0].classList.add("display-none");
+	document.getElementsByClassName('loading')[0].classList.add("display-none");
+	if(document.getElementsByTagName('canvas')[0])
+	document.getElementsByTagName('canvas')[0].style.height = '0px';
+	  
+	!function(d,s,id){
+		var js: any,
+			fjs=d.getElementsByTagName(s)[0],
+			p='https';
+		if(!d.getElementById(id)){
+			js=d.createElement(s);
+			js.id=id;
+			js.src=p+"://platform.twitter.com/widgets.js";
+			fjs.parentNode.insertBefore(js,fjs);
+		}
+	}
+	(document,"script","twitter-wjs");
   }
 
 }
